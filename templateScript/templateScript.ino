fd34b733206ffb45ad1471fd22fd725568281173
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-02-02 04:39:23 +0100

    WiFi web server
    Add Wifi web server to gather wifi credentials

--- templateScript/templateScript.ino
+++ templateScript/templateScript.ino
@@ -1,42 +1,55 @@
 #include <ESP8266WiFi.h>
-#include "arduino_secrets.h"
+#include <ESP8266WebServer.h>
+#include <EEPROM.h>
+ESP8266WebServer server(80);
 
-const char ssid[] = SECRET_SSID;
-const char password[] = SECRET_PASS;
-const char id[] = SECRET_ID;
+const char id[] = "AuraHub_27d435b37a58";
+struct settings
+{
+    char ssid[30];
+    char password[30];
+
+} user_wifi = {};
 
 void setup_wifi()
 {
-    delay(10);
-
+    EEPROM.get(0, user_wifi);
     Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
+    Serial.print("Trying to connect to ");
+    Serial.println(user_wifi.ssid);
 
     WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
+    WiFi.begin(user_wifi.ssid, user_wifi.password);
 
+    byte tries = 0;
     while (WiFi.status() != WL_CONNECTED)
     {
         digitalWrite(BUILTIN_LED, LOW);
-        delay(500);
-        Serial.print(".");
-        digitalWrite(BUILTIN_LED, HIGH);
-        delay(250);
-    }
 
-    Serial.println("");
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
-    digitalWrite(BUILTIN_LED, LOW);
+        delay(600);
+        Serial.print(".");
+
+        if (tries++ > 10)
+        {
+            WiFi.mode(WIFI_AP);
+            WiFi.softAP(id, "");
+            break;
+        }
+        digitalWrite(BUILTIN_LED, HIGH);
+
+        delay(400);
+    }
+    server.on("/", handlePortal);
+    server.begin();
 }
 
 void setup()
 {
     pinMode(BUILTIN_LED, OUTPUT);
     Serial.begin(115200);
+    delay(100);
 
+    EEPROM.begin(sizeof(struct settings));
     setup_wifi();
 
     digitalWrite(BUILTIN_LED, HIGH);
@@ -44,4 +57,25 @@ void setup()
 
 void loop()
 {
+    server.handleClient();
+}
+
+void handlePortal()
+{
+    if (server.method() == HTTP_POST)
+    {
+        strncpy(user_wifi.ssid, server.arg("ssid").c_str(), sizeof(user_wifi.ssid));
+        strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password));
+        user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
+        EEPROM.put(0, user_wifi);
+        EEPROM.commit();
+        WiFi.mode(WIFI_STA);
+        WiFi.begin(user_wifi.ssid, user_wifi.password);
+
+        server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!</main></body></html>");
+    }
+    else
+    {
+        server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
+    }
 }
