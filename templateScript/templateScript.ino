c2931c3146d723a9522e334b614a57034ac5e221
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2024-02-03 22:55:10 +0100

    Add Captive Portal

--- templateScript/templateScript.ino
+++ templateScript/templateScript.ino
@@ -1,11 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
 #include <EEPROM.h>
+#include <DNSServer.h>
+
 ESP8266WebServer server(80);
+DNSServer dnsServer;
 
 const char id[] = "AuraHub_27d435b37a58";
 bool setupIsRunning = false;
 bool accesPointIsRunning = false;
+
+IPAddress apIP(192, 168, 1, 1);
+const byte DNS_PORT = 53;
+
+String formResponseHTML = "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/post' method='post'> <h1 class=''>Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>";
 
 struct settings
 {
@@ -44,7 +52,18 @@ void setup_wifi()
             Serial.println("Starting Access Point Mode");
             digitalWrite(BUILTIN_LED, LOW);
             WiFi.mode(WIFI_AP);
-            WiFi.softAP(id, "");
+            WiFi.softAP(id);
+
+            dnsServer.start(DNS_PORT, "*", apIP);
+
+            // Turn on web server and wait
+            server.on("/", handleForm);
+            server.on("/post", handlePost);
+
+            server.onNotFound([]()
+                              { server.send(200, "text/html", formResponseHTML); });
+            server.begin();
+
             break;
         }
         digitalWrite(BUILTIN_LED, HIGH);
@@ -61,10 +80,6 @@ void setup_wifi()
         digitalWrite(BUILTIN_LED, HIGH);
     }
 
-    // Turn on web server and wait
-    server.on("/", handlePortal);
-    server.begin();
-
     setupIsRunning = false;
 }
 
@@ -79,18 +94,16 @@ void setup()
     EEPROM.get(0, user_wifi);
 
     // Setup access point config
-    IPAddress localIP(192, 168, 1, 1);
-    IPAddress gateway(192, 168, 1, 0);
-    IPAddress subnet(255, 255, 255, 0);
-    WiFi.softAPConfig(localIP, gateway, subnet);
+    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
 
     setup_wifi();
 }
 
 void loop()
 {
-    // Handle web server request
+    // Handle web server and DNS request
     server.handleClient();
+    dnsServer.processNextRequest();
 
     // If not connected to wifi run wifi setup sequence
     if (WiFi.status() != WL_CONNECTED)
@@ -102,14 +115,15 @@ void loop()
     }
 }
 
-void handlePortal()
+void handlePost()
 {
-    if (server.method() == HTTP_POST)
-    {
+
         // Parse data from HTTP POST request
         strncpy(user_wifi.ssid, server.arg("ssid").c_str(), sizeof(user_wifi.ssid));
         strncpy(user_wifi.password, server.arg("password").c_str(), sizeof(user_wifi.password));
         user_wifi.ssid[server.arg("ssid").length()] = user_wifi.password[server.arg("password").length()] = '\0';
+
+        server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!</main></body></html>");
 
         // Put data to EEPROM memory
         EEPROM.put(0, user_wifi);
@@ -117,11 +131,14 @@ void handlePortal()
 
         // Rerun wifi setup sequence after getting new data
         setup_wifi();
+}
 
-        server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup</h1> <br/> <p>Your settings have been saved successfully!</main></body></html>");
-    }
-    else
-    {
-        server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup</title> <style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{cursor: pointer;border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1{text-align: center}</style> </head> <body><main class='form-signin'> <form action='/' method='post'> <h1 class=''>Wifi Setup</h1><br/><div class='form-floating'><label>SSID</label><input type='text' class='form-control' name='ssid'> </div><div class='form-floating'><br/><label>Password</label><input type='password' class='form-control' name='password'></div><br/><br/><button type='submit'>Save</button><p style='text-align: right'></p></form></main> </body></html>");
-    }
+void handleForm()
+{
+    server.send(200, "text/html", formResponseHTML);
+}
+
+void handleSuccess()
+{
+    server.send(200, "text/html", "<!doctype html><html lang='en'><head><meta charset='utf-8'><meta name='viewport' content='width=device-width, initial-scale=1'><title>Wifi Setup Success</title><style>*,::after,::before{box-sizing:border-box;}body{margin:0;font-family:'Segoe UI',Roboto,'Helvetica Neue',Arial,'Noto Sans','Liberation Sans';font-size:1rem;font-weight:400;line-height:1.5;color:#212529;background-color:#f5f5f5;}.form-control{display:block;width:100%;height:calc(1.5em + .75rem + 2px);border:1px solid #ced4da;}button{border:1px solid transparent;color:#fff;background-color:#007bff;border-color:#007bff;padding:.5rem 1rem;font-size:1.25rem;line-height:1.5;border-radius:.3rem;width:100%}.form-signin{width:100%;max-width:400px;padding:15px;margin:auto;}h1,p{text-align: center}</style> </head> <body><main class='form-signin'> <h1>Wifi Setup Success</h1> <br/> <p>Your settings have been saved successfully!</main></body></html>");
 }
